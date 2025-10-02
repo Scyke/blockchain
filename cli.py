3aea85a0b573957b6383d5e30ead10bb1dd4964c
Sid Natu <natusiddhant@gmail.com>
2025-10-02 14:49:12 -0700

    finished mine and transaction cmds

--- cli.py
+++ cli.py
@@ -17,7 +17,7 @@ def load_state():
 
 def save_state(node_id, chain):
     with STATE_FILE.open('w') as f:
-        json.dump({'node_id': node_id, "chain": chain}, f)
+        json.dump({'node_id': node_id, "chain": chain}, f, indent=2)
         
 def verify_pow(last_proof: int, proof: int, zeros: int) -> bool:
     guess = f'{last_proof}{proof}'.encode()
@@ -27,10 +27,61 @@ def verify_pow(last_proof: int, proof: int, zeros: int) -> bool:
 
 def proof_of_work_offload(last_proof: int, zeros: int = 4, port: str = None, start_nonce: int = 0, batch_size: int = 500_000) -> int:
     #Should have UART protocol, want to test with CPU mining for now
-    proof = 0;
+    proof = start_nonce;
     
     while True:
         if verify_pow(last_proof, proof, zeros):
             return proof
         proof += 1;
+        
+def cmd_mine(args):
+    node_id, persisted_chain = load_state();
+    blk = Blockchain()
+    if persisted_chain:
+        blk.chain = persisted_chain; #load previous chain
+        
+    last_block = blk.last_block;
+    last_proof = blk.last_block["proof"];
+    
+    if args.use_uart: #if im using UART, else use CPU mining
+        proof = proof_of_work_offload(last_proof, zeros=args.zeros, port=args.port, start_nonce=args.start_nonce, batch_size=args.batch)
+    else:
+        proof = blk.proof_of_work(last_proof);
+        
+    blk.new_transaction(sender='0', recipient=node_id, amount=1); #one proof is cracked, prepare a reward
+    
+    previous_hash = blk.hash(last_block);
+    
+    block = blk.new_block(proof, previous_hash=previous_hash);
+    
+    save_state(node_id, blk.chain);
+    
+    out = {
+        "message": 'New Block mined',
+        "index": block['index'],
+        "transactions": block['transactions'],
+        'proof': block['proof'],
+        "previous_hash": block["previous_hash"],
+    }
+    print(json.dumps(out, indent=2))
+    
+def cmd_tx(args):
+    node_id, persisted_chain = load_state();
+    
+    blk = Blockchain()
+    
+    if persisted_chain:
+        blk.chain = persisted_chain;
+        
+    #validating inputs
+    for k in ('sender', 'recipient', 'amount'):
+        if getattr(args, k) is None:
+            print(f"Missing --{k}", file=sys.stderr)
+            sys.exit(1);
+    index = blk.new_transaction(args.sender, args.recipient, args.amount)
+    save_state(node_id, blk.chain)
+    print(json.dumps({"message": f'Transaction will be added to Block {index}'}))
+    
+    
+    
     
